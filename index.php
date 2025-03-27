1579299722da63c9a6409481218f65ee95780a82
Alisson Alencar <alissonsts910@gmail.com>
2025-03-27 16:30:38 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+    <h1>Testando o MAMP</h1>
+    <?php
+    $nome = "Alisson";
+    echo "Olá, $nome";
+    phpinfo()
+    ?>
+</body>
+</html>
